7867aaffc32c79401498fe443649f209d98a9e34
Jan Martin Reckel <jm.reckel@t-online.de>
2025-04-20 15:29:21 +0200

    Remove duplicate initialization of `tauri_plugin_shell`

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -17,7 +17,6 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_clipboard_manager::init())
         .manage(terminal_state)
